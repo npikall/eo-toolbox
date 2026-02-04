12240139865b7cf1dad6326c2a9d2fcc4dd95bb9
npikall <nikolas.pikall@gmail.com>
2026-02-04 14:35:39 +0100

    feat: add name and version to __init__

--- src/eo_toolbox/__init__.py
+++ src/eo_toolbox/__init__.py
@@ -1,5 +1,12 @@
-# ruff: noqa: D104
+"""eo-toolbox: A Package to..."""
+
+from importlib.metadata import version
+
 from eo_toolbox import calc, index, mask, stac
+
+__version__ = version(__name__)
+
+name = "eo_toolbox"
 
 __all__ = [
     "calc",
